d4400d62f3b5fba8b3e5193b1f7bedc0599a847b
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-06-21 14:41:51 +0800

    Add reactive and visible todo list

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from 'react';
+import { useState } from "react";
 
 function App() {
   const [todoList, setTodoList] = useState([]);
@@ -7,26 +7,44 @@ function App() {
 
   const handleInput = (event) => {
     setNewTask(event.target.value);
-  }
+  };
 
   const addTask = () => {
-    const newTodoList = [...todoList, newTask];
-    setTodoList(newTodoList);
+    setTodoList([...todoList, newTask]);
     setNewTask("");
-    console.log("New Todo List: " + newTodoList);
-  }
+    console.log("New Todo List: " + [...todoList, newTask]); // to check
+  };
+
+  const deleteTask = (taskName) => {
+    setTodoList(todoList.filter((task) => task !== taskName));
+    console.log(
+      "New Todo List: " + todoList.filter((task) => task !== taskName)
+    ); // to check
+  };
 
   return (
     <div className="App">
       <div className="addTask">
-        <input 
-          value={newTask} 
-          onChange={handleInput} 
-          type="text" 
-          placeholder="Enter a new task here..." />
+        <input
+          value={newTask}
+          onChange={handleInput}
+          type="text"
+          placeholder="Enter a new task here..."
+        />
         <button onClick={addTask}> Add Task </button>
       </div>
-      <div className="list"></div>
+      <ul className="list">
+        {todoList.map((task, index) => {
+          return (
+            <li key={task + index}>
+              <div style={{ display: "flex" }}>
+                {task}
+                <button onClick={() => deleteTask(task)}> X </button>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </div>
   );
 }
