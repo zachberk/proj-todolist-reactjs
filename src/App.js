f9fa7d0af18d7067f4458cffdb04dbf5325371c4
Zachary Berkenkotter <109328944+zachberk@users.noreply.github.com>
2024-06-21 13:53:22 +0800

    Set up updating input field and button

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import "./App.css";
+import { useState } from 'react';
 
 function App() {
+  const [todoList, setTodoList] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const handleInput = (event) => {
+    setNewTask(event.target.value);
+  }
+
+  const addTask = () => {
+    const newTodoList = [...todoList, newTask];
+    setTodoList(newTodoList);
+    setNewTask("");
+    console.log("New Todo List: " + newTodoList);
+  }
+
   return (
     <div className="App">
-    
+      <div className="addTask">
+        <input 
+          value={newTask} 
+          onChange={handleInput} 
+          type="text" 
+          placeholder="Enter a new task here..." />
+        <button onClick={addTask}> Add Task </button>
+      </div>
+      <div className="list"></div>
     </div>
   );
 }
